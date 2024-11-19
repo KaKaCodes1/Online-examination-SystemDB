65ebbae6593e7807ffc74622d2b7ce64e18b998f
kevin koech <136765569+kevin-ada@users.noreply.github.com>
2024-11-19 18:52:35 +0300

    Create validate_exams_trigger.sql

---
+++ validate_exams_trigger.sql
@@ -0,0 +1,11 @@
+DELIMITER //
+CREATE TRIGGER validate_exam_date
+BEFORE INSERT ON Exam
+FOR EACH ROW
+BEGIN
+    IF NEW.date < CURDATE() THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Exam date cannot be in the past';
+    END IF;
+END;//
+DELIMITER ;
